6115913b3e39c7bd079892c83b3a92f3cb761234
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-06-20 14:32:21 +0200

    [add] style in home dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,27 +2,34 @@
 
 @section('content')
     <div class="container">
-        <h1>Home Admin</h1>
-        <div class="row">
+        <div class="row m-4">
             <div class="col">
-                <div class="card m-auto w-50">
-                    @if ($restaurant->image)
-                        <img src="{{ $restaurant->image }}" class="card-img-top" alt="{{ $restaurant->business_name }}">
-                    @else
-                        <img src="{{ Vite::asset('resources/img/placeholder.jpg') }}">
-                    @endif
+                <div class="card">
+                    <div class="card-img-top card-img-box">
+                        @if ($restaurant->image)
+                            <img src="{{ $restaurant->image }}" alt="{{ $restaurant->business_name }}">
+                        @else
+                            <img src="{{ Vite::asset('resources/img/placeholder.jpg') }}">
+                        @endif
+                    </div>
+
 
                     <div class="card-body">
 
-                        @foreach ($restaurant->types as $type)
-                            <span class="badge text-bg-primary">{{ $type->name }}</span>
-                        @endforeach
+                        <div class="badge-container">
+                            @foreach ($restaurant->types as $type)
+                                <span class="badge badge-custom-secondary">{{ $type->name }}</span>
+                            @endforeach
+                        </div>
 
                         <h3 class="card-title">{{ $restaurant->business_name }}</h3>
-                        <p class="card-text">{{ $restaurant->email }}</p>
-                        <p class="card-text">{{ $restaurant->address }}</p>
-                        <p class="card-text">{{ $restaurant->vat_number }}</p>
-                        <p class="card-text">{{ $restaurant->phone_number }}</p>
+                        <p class="card-text"><span class="fst-italic">Indirizzo: </span>Milano, {{ $restaurant->address }}
+                        </p>
+                        <p class="card-text"><span class="fst-italic">Contatto email: </span>{{ $restaurant->email }}</p>
+                        <p class="card-text"><span class="fst-italic">Contatto telefonico: </span>
+                            + 39 {{ $restaurant->phone_number }}
+                        </p>
+                        <p class="card-text"><span class="fst-italic">P.IVA: </span>{{ $restaurant->vat_number }}</p>
                     </div>
                 </div>
             </div>
