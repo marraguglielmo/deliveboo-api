0a29af0e1775d9ea9c54787af2ecca2931b8d878
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-06-26 14:42:06 +0200

    [add] style in thankyou page and rejectpayment pages

--- resources/views/rejectpayment.blade.php
+++ resources/views/rejectpayment.blade.php
@@ -1,1 +1,15 @@
-<h3>Pagamento rifiutato</h3>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container vh-100 d-flex justify-content-center align-items-center">
+        <div class="reject-box">
+            <i class="fa-solid fa-circle-xmark"></i>
+            <h3>Il pagamento è stato rifiutato</h3>
+            <div>
+                <a href="{{ env('APP_FRONTEND_URL') . '/checkout' }} " class="btn btn-custom-primary w-auto"> Torna al
+                    checkout</a>
+                <a href="{{ env('APP_FRONTEND_URL') }}" class="btn btn-custom-secondary w-auto">Torna al sito</a>
+            </div>
+        </div>
+    </div>
+@endsection
