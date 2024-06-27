431ba4be065c5cc9b69d60bca7d2b241a70a400d
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-06-27 10:49:34 +0200

    [add] statistics

--- app/Http/Controllers/Admin/OrderStatisticsController.php
+++ app/Http/Controllers/Admin/OrderStatisticsController.php
@@ -17,73 +17,86 @@ class OrderStatisticsController extends Controller
         // Ordini univoci fatti al ristorante loggato
         $orders = Order::whereHas('dishes', function ($query) {
             $query->where('restaurant_id', Auth::user()->id);
-        })->with('dishes')->get();
+        })->where('status', 1)->with('dishes')->get();
 
         $monthsOrders = [
             [
                 'year' => '23',
                 'month' => '06',
                 'quantity' => 0,
+                'orders_amount' => 0
             ],
             [
                 'year' => '23',
                 'month' => '07',
                 'quantity' => 0,
+                'orders_amount' => 0
             ],
             [
                 'year' => '23',
                 'month' => '08',
                 'quantity' => 0,
+                'orders_amount' => 0
             ],
             [
                 'year' => '23',
                 'month' => '09',
                 'quantity' => 0,
+                'orders_amount' => 0
             ],
             [
                 'year' => '23',
                 'month' => '10',
                 'quantity' => 0,
+                'orders_amount' => 0
             ],
             [
                 'year' => '23',
                 'month' => '11',
                 'quantity' => 0,
+                'orders_amount' => 0
             ],
             [
                 'year' => '23',
                 'month' => '12',
                 'quantity' => 0,
+                'orders_amount' => 0
             ],
             [
                 'year' => '24',
                 'month' => '01',
                 'quantity' => 0,
+                'orders_amount' => 0
             ],
             [
                 'year' => '24',
                 'month' => '02',
                 'quantity' => 0,
+                'orders_amount' => 0
             ],
             [
                 'year' => '24',
                 'month' => '03',
                 'quantity' => 0,
+                'orders_amount' => 0
             ],
             [
                 'year' => '24',
                 'month' => '04',
                 'quantity' => 0,
+                'orders_amount' => 0
             ],
             [
                 'year' => '24',
                 'month' => '05',
                 'quantity' => 0,
+                'orders_amount' => 0
             ],
             [
                 'year' => '24',
                 'month' => '06',
                 'quantity' => 0,
+                'orders_amount' => 0
             ]
         ];
 
@@ -95,22 +108,28 @@ class OrderStatisticsController extends Controller
             foreach ($monthsOrders as &$monthOrder) {
                 if ($year == $monthOrder['year'] && $month == $monthOrder['month']) {
                     $monthOrder['quantity']++;
+                    $monthOrder['orders_amount'] = $monthOrder['orders_amount'] + $order['total_price'];
                     break;
                 }
             }
         }
 
         $orders_num = [];
+        $orders_amount = [];
 
         foreach ($monthsOrders as &$monthOrder) {
             array_push($orders_num, $monthOrder['quantity']);
         }
 
+        foreach ($monthsOrders as &$monthOrder) {
+            array_push($orders_amount, $monthOrder['orders_amount']);
+        }
+
         $statistics = [
-            'orders' => $orders_num,
+            'orders_num' => $orders_num,
+            'orders_amount' => $orders_amount,
         ];
 
-        // Passa i dati alla vista
         return view('statistics.index', compact('statistics'));
     }
 }
--- resources/views/statistics/index.blade.php
+++ resources/views/statistics/index.blade.php
@@ -1,20 +1,28 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container">
-        <h2>Grafici</h2>
-        <div>
-            <canvas id="orderChart"></canvas>
+    <div class="container my-4">
+        <div class="chart-box">
+            <h3 class="text-center">Numero di ordini al mese</h3>
+            <div>
+                <canvas id="ordersNumChart" height="40vh" width="80vw"></canvas>
+            </div>
+        </div>
+
+        <div class="chart-box">
+            <h3 class="text-center">Ammontare vendite al mese</h3>
+            <div>
+                <canvas id="ordersAmountChart" height="40vh" width="80vw"></canvas>
+            </div>
         </div>
     </div>
 
     <script>
-        let ctx = document.getElementById('orderChart');
-
-        let datasets = [];
+        let ctxNum = document.getElementById('ordersNumChart');
+        let ctxAmount = document.getElementById('ordersAmountChart');
         const statistics = @json($statistics);
 
-        new Chart(ctx, {
+        new Chart(ctxNum, {
             type: 'bar',
             data: {
                 labels: ['Giugno 2023', 'Luglio 2023', 'Agosto 2023', 'Settembre 2023', 'Ottobre 2023',
@@ -22,9 +30,35 @@
                     'Febbraio 2024', 'Marzo 2024', 'Aprile 2024', 'Maggio 2024', 'Giugno 2024'
                 ],
                 datasets: [{
-                    label: 'numero di ordini',
-                    data: statistics.orders,
-                    borderWidth: 1
+                    label: '# ordini',
+                    data: statistics.orders_num,
+                    borderWidth: 1,
+                    backgroundColor: 'rgba(255, 159, 28, 0.8)',
+                    hoverBackgroundColor: 'rgb(255, 72, 0)'
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+
+        new Chart(ctxAmount, {
+            type: 'bar',
+            data: {
+                labels: ['Giugno 2023', 'Luglio 2023', 'Agosto 2023', 'Settembre 2023', 'Ottobre 2023',
+                    'Novembre 2023', 'Dicembre 2023', 'Gennaio 2024',
+                    'Febbraio 2024', 'Marzo 2024', 'Aprile 2024', 'Maggio 2024', 'Giugno 2024'
+                ],
+                datasets: [{
+                    label: 'vendite €',
+                    data: statistics.orders_amount,
+                    borderWidth: 1,
+                    backgroundColor: 'rgba(255, 159, 28, 0.8)',
+                    hoverBackgroundColor: 'rgb(255, 72, 0)'
                 }]
             },
             options: {
